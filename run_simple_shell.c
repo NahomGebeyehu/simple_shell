356e18821ff89ab97279484088dab7227f270db6
NahomGebeyehu <gebeyehuay@gmail.com>
2023-11-15 20:19:20 +0300

    Liver

--- run_simple_shell.c
+++ run_simple_shell.c
@@ -1,113 +1,98 @@
+/* run_simple_shell.c */
 #include "shell.h"
-#include <stdlib.h>
 
-/**
- * handle_ctrl_c - Handles the Ctrl-C signal
- * @signo: The signal number
- */
-void handle_ctrl_c(int signo)
-{
-	if (signo == SIGINT)
-	{
-		write(STDOUT_FILENO, "\n", 1);
-		exit_shell();
-	}
-}
+char *buffer = NULL;
 
-/**
- * run_simple_shell - Runs a simple shell
- * @interactive: Flag to check if the shell is interactive
- * Return: 0 on success
- */
+int is_absolute_path(const char *path);
+
 int run_simple_shell(int interactive)
 {
-	char *buffer = NULL;
-	ssize_t characters;
-	char *path;
-	size_t len = 0;
+    ssize_t characters;
+    char *path;
+    size_t len = 0;
 
-	path = getenv("PATH");
-	signal(SIGINT, handle_ctrl_c);
+    path = getenv("PATH");
 
-	while (1)
-	{
-		char **args;
-		pid_t child_pid;
-		int status;
+    signal(SIGINT, SIG_IGN);
 
-		if (interactive)
-			write(STDOUT_FILENO, "$ ", 2);
+    while (1)
+    {
+        pid_t child_pid;
+        int status;
 
-		characters = getline(&buffer, &len, stdin);
+        if (interactive && isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-		if (characters == -1)
-		{
-			if (interactive && isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			free(buffer);
-			exit_shell();
-		}
+        characters = getline(&buffer, &len, stdin);
 
-		buffer[characters - 1] = '\0'; /* Remove newline character */
+        if (characters == -1)
+        {
+            if (interactive && isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "\n", 1);
+            free(buffer);
+            exit_shell();
+        }
 
-		if (characters > 1) /* Ignore empty lines */
-		{
-			args = parse_input(buffer);
+        buffer[characters - 1] = '\0'; /* Remove newline character */
 
-			if (_strcmp(args[0], "exit") == 0)
-			{
-				free(args);
-				exit_shell();
-			}
-			else if (_strcmp(args[0], "env") == 0)
-			{
-				print_environment();
-			}
-			else if (!is_absolute_path(args[0]))
-			{
-				char *full_path = search_path(args[0], path);
+        if (characters > 1)
+        {
+            char **args = parse_input(buffer);
 
-				if (full_path != NULL)
-				{
-					args[0] = full_path;
-				}
-				else
-				{
-					if (interactive)
-					{
-						write(STDERR_FILENO, "sh: 1: ", 6);
-						write(STDERR_FILENO, args[0], _strlen(args[0]));
-						write(STDERR_FILENO, ": not found\n", 12);
-					}
-					free(args);
-					continue;
-				}
-			}
+            if (_strcmp(args[0], "exit") == 0)
+            {
+                free(args);
+                exit_shell();
+            }
+            else if (_strcmp(args[0], "env") == 0)
+            {
+                print_environment();
+            }
+            else if (!is_absolute_path(args[0]))
+            {
+                char *full_path = search_path(args[0], path);
 
-			child_pid = fork();
-			if (child_pid == -1)
-			{
-				perror("Error forking");
-			}
-			else if (child_pid == 0)
-			{
-				/* Child process */
-				if (execve(args[0], args, NULL) == -1)
-				{
-					perror("Execution error");
-					_exit(EXIT_FAILURE);
-				}
-			}
-			else
-			{
-				/* Parent process */
-				waitpid(child_pid, &status, 0);
-			}
+                if (full_path != NULL)
+                {
+                    args[0] = full_path;
+                }
+                else
+                {
+                    if (interactive)
+                    {
+                        write(STDERR_FILENO, "sh: 1: ", 6);
+                        write(STDERR_FILENO, args[0], _strlen(args[0]));
+                        write(STDERR_FILENO, ": not found\n", 12);
+                    }
+                    free(args);
+                    continue;
+                }
+            }
 
-			free(args);
-		}
-	}
-	free(buffer);
-	return (0);
+            child_pid = fork();
+            if (child_pid == -1)
+            {
+                perror("Error forking");
+            }
+            else if (child_pid == 0)
+            {
+                /* Child process */
+                if (execve(args[0], args, NULL) == -1)
+                {
+                    perror("sh");
+                    _exit(EXIT_FAILURE);
+                }
+            }
+            else
+            {
+                /* Parent process */
+                waitpid(child_pid, &status, 0);
+            }
+
+            free(args);
+        }
+    }
+
+    return 0;
 }
 
