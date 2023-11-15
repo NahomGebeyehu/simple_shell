ad576d87789b0131c4050847b3aa82ae949a25d6
NahomGebeyehu <gebeyehuay@gmail.com>
2023-11-15 22:49:07 +0300

    bety

--- run_simple_shell.c
+++ run_simple_shell.c
@@ -3,95 +3,101 @@
 
 int is_absolute_path(const char *path);
 
+/**
+ * run_simple_shell - Runs a simple shell
+ * @interactive: Flag to check if shell is interactive
+ * Return: 0 on success
+ */
 int run_simple_shell(int interactive)
 {
-  char *buffer = NULL;
-  ssize_t characters;
-  char *path;
-  size_t len = 0;
+	char *buffer = NULL;
+	ssize_t characters;
+	char *path;
+	size_t len = 0;
 
-  path = getenv("PATH");
+	path = getenv("PATH");
 
-  signal(SIGINT, SIG_IGN);
+	signal(SIGINT, SIG_IGN);
 
-  while (1)
-  {
-    char **args;
-    pid_t child_pid;
-    int status;
+	while (1)
+	{
+		char **args;
+		pid_t child_pid;
+		int status;
 
-    if (interactive)
-      write(STDOUT_FILENO, ":) ", 3);
+		if (interactive)
+			write(STDOUT_FILENO, "$ ", 2);
 
-    characters = getline(&buffer, &len, stdin);
+		characters = getline(&buffer, &len, stdin);
 
-    if (characters == -1)
-    {
-      if (interactive && isatty(STDIN_FILENO))
-        write(STDOUT_FILENO, "\n", 1);
-      free(buffer);
-      exit_shell();
-    }
+		if (characters == -1)
+		{
+			if (interactive && isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			free(buffer);
+			exit_shell();
+		}
 
-    buffer[characters - 1] = '\0'; /* Remove newline character */
+		buffer[characters - 1] = '\0'; /* Remove newline character */
 
-    if (characters > 1) /* Ignore empty lines */
-    {
-      args = parse_input(buffer);
+		if (characters > 1) /* Ignore empty lines */
+		{
+			args = parse_input(buffer);
 
-      if (_strcmp(args[0], "exit") == 0)
-      {
-        free(args);
-        exit_shell();
-      }
-      else if (_strcmp(args[0], "env") == 0)
-      {
-        print_environment();
-      }
-      else if (!is_absolute_path(args[0]))
-      {
-        char *full_path = search_path(args[0], path);
+			if (_strcmp(args[0], "exit") == 0)
+			{
+				free(args);
+				exit_shell();
+			}
+			else if (_strcmp(args[0], "env") == 0)
+			{
+				print_environment();
+			}
+			else if (!is_absolute_path(args[0]))
+			{
+				char *full_path = search_path(args[0], path);
 
-        if (full_path != NULL)
-        {
-          args[0] = full_path;
-        }
-        else
-        {
-          if (interactive)
-          {
-            write(STDERR_FILENO, "sh: 1: ", 6);
-            write(STDERR_FILENO, args[0], _strlen(args[0]));
-            write(STDERR_FILENO, ": not found\n", 12);
-          }
-          free(args);
-          continue;
-        }
-      }
+				if (full_path != NULL)
+				{
+					args[0] = full_path;
+				}
+				else
+				{
+					if (interactive)
+					{
+						write(STDERR_FILENO, "sh: 1: ", 6);
+						write(STDERR_FILENO, args[0], _strlen(args[0]));
+						write(STDERR_FILENO, ": not found\n", 12);
+					}
+					free(args);
+					continue;
+				}
+			}
 
-      child_pid = fork();
-      if (child_pid == -1)
-      {
-        perror("Error forking");
-      }
-      else if (child_pid == 0)
-      {
-        /* Child process */
-        if (execve(args[0], args, NULL) == -1)
-        {
-          perror("Execution error");
-          _exit(EXIT_FAILURE);
-        }
-      }
-      else
-      {
-        /* Parent process */
-        waitpid(child_pid, &status, 0);
-      }
+			child_pid = fork();
+			if (child_pid == -1)
+			{
+				perror("Error forking");
+			}
+			else if (child_pid == 0)
+			{
+				/* Child process */
+				if (execve(args[0], args, NULL) == -1)
+				{
+					perror("Execution error");
+					_exit(EXIT_FAILURE);
+				}
+			}
+			else
+			{
+				/* Parent process */
+				waitpid(child_pid, &status, 0);
+			}
 
-      free(args);
-    }
-  }
-
-  return (0);
+			free(args);
+		}
+	}
+	free(buffer);
+	return (0);
 }
+
