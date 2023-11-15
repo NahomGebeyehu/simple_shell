b9d1acc9b7137cec037a3fbd77fc8e9dbed9df19
NahomGebeyehu <gebeyehuay@gmail.com>
2023-11-15 16:12:53 +0300

    Fixed betty

--- strlen.c
+++ strlen.c
@@ -7,26 +7,28 @@
  */
 size_t _strlen(const char *s)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (s[len] != '\0')
-        len++;
+	while (s[len] != '\0')
+		len++;
 
-    return len;
+	return (len);
 }
+
 /**
  * _strcmp - This is the custom implementation of strcmp
  * @s1: First string
  * @s2: Second string
- * Return: This returns 0 if strings are equal, 
+ * Return: This returns 0 if strings are equal,
  * positive if s1 > s2, negative if s1 < s2
  */
 int _strcmp(const char *s1, const char *s2)
 {
-    while (*s1 && (*s1 == *s2))
-    {
-        s1++;
-        s2++;
-    }
-    return *(unsigned char *)s1 - *(unsigned char *)s2;
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
+
